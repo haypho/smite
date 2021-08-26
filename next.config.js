6aa1cb9b9c476f005df15a64f078f738fab33207
Hayden Phothong <hphothong@gmail.com>
2021-08-25 18:32:06 -0600

    Add github pages deploy scripts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
+const app = require("./package.json");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['webcdn.hirezstudios.com'],
   },
+  basePath: process.env.BASE_PATH,
+  assetPrefix: app.homepage,
 }
