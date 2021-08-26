375d94dc981157eadf9082aef298bfad3d9cbfca
hphothong <hphothong@gmail.com>
2021-08-26 14:10:13 -0600

    Fix assetPrefix and basePath configurations

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-const app = require("./package.json");
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['webcdn.hirezstudios.com'],
   },
-  basePath: process.env.BASE_PATH,
-  assetPrefix: app.homepage,
+  basePath: process.env.BASE_PATH ?? "",
+  assetPrefix: process.env.ASSET_PREFIX ?? "",
 }
