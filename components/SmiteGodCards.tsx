67535c96609896bd2c0fb0424bd33bdfae781e14
Saurabh <140235723+FullStackSorcerer@users.noreply.github.com>
2023-07-29 22:03:42 +0530

    fix: add missing no results message (#17)
    Co-authored-by: Hayden Phothong <hphothong@gmail.com>

--- components/SmiteGodCards.tsx
+++ components/SmiteGodCards.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Container, Typography } from "@mui/material";
 import styled from "@emotion/styled";
 import { useSelector } from "react-redux";
 import { RootState } from "../stores";
@@ -19,40 +19,53 @@ export const SmiteGodCards = () => {
   );
 
   return (
-    <Box
-      display="flex"
-      flex={1}
-      justifyContent="center"
-      marginTop={5}
-      flexWrap="wrap"
-      gap={5}
-      height="fit-content"
-    >
-      {randomGods.map((god) => (
-        <SmiteGodCard
-          key={god.id}
-          sx={{
-            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${god.godCard_URL})`,
-          }}
+    randomGods.length ? (
+      <Box
+        display="flex"
+        flex={1}
+        justifyContent="center"
+        marginTop={5}
+        flexWrap="wrap"
+        gap={5}
+        height="fit-content"
+      >
+        {randomGods.map((god) => (
+          <SmiteGodCard
+            key={god.id}
+            sx={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${god.godCard_URL})`,
+            }}
+          >
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h4"
+                color="white"
+                fontWeight={700}
+              >
+                {god.Name}
+              </Typography>
+              <Typography color="white" fontWeight={700}>
+                {god.Pantheon}
+              </Typography>
+              <Typography color="white" fontWeight={700}>
+                {god.Title}
+              </Typography>
+            </CardContent>
+          </SmiteGodCard>
+        ))}
+      </Box>
+    ) : (
+      <Container sx={{ height: 'calc(90vh - 64px)' }}>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height="100%"
         >
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h4"
-              color="white"
-              fontWeight={700}
-            >
-              {god.Name}
-            </Typography>
-            <Typography color="white" fontWeight={700}>
-              {god.Pantheon}
-            </Typography>
-            <Typography color="white" fontWeight={700}>
-              {god.Title}
-            </Typography>
-          </CardContent>
-        </SmiteGodCard>
-      ))}
-    </Box>
+          <Typography color="gray">No results found.</Typography>
+        </Box>
+      </Container>
+    )
   );
 };
